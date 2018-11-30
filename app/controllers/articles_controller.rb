0c714963c23d64442bb581d78ea4f74bc498313e
Alicia Baker <aliciabaker314@gmail.com>
2018-11-30 10:48:07 -0700

    update users table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,10 @@
 class ArticlesController < ApplicationController
-
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -11,15 +12,13 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-
   end
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
-      #do something
-      flash[:success] = "Article Created"
+      flash[:success] = "Article was created successfully"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -27,9 +26,8 @@ class ArticlesController < ApplicationController
   end
 
   def update
-
     if @article.update(article_params)
-      flash[:success] = "Article Updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -37,24 +35,28 @@ class ArticlesController < ApplicationController
   end
 
   def show
-
   end
 
   def destroy
-
     @article.destroy
-    flash[:danger] = "Article Removed"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
-
   private
-  def article_params
-    params.require(:article).permit(:title, :description)
-  end
 
   def set_article
     @article = Article.find(params[:id])
   end
 
+  def article_params
+    params.require(:article).permit(:title, :description, category_ids: [])
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to root_path
+    end
+  end
 end
