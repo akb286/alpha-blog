23b6444781540015e8ba50443c24a16332c7d770
Alicia Baker <aliciabaker314@gmail.com>
2018-11-30 17:37:41 -0700

    dropped then readded users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_220450) do
+ActiveRecord::Schema.define(version: 2018_12_01_002650) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
