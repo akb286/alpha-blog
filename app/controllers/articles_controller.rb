0ac990ece625d30f518d547c51ad377244d8139f
Alicia Baker <aliciabaker314@gmail.com>
2018-12-02 16:43:55 -0700

    removed require_user for now

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,5 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-  before_action :require_user, except: [:index, :show]
-  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @article = Article.all
@@ -54,10 +52,4 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :description, category_ids: [])
   end
 
-  def require_same_user
-    if current_user != @article.user && !current_user.admin?
-      flash[:danger] = "You can only edit or delete your own article"
-      redirect_to root_path
-    end
-  end
 end
