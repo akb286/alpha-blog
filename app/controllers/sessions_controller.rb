4778211a7b151bc40000ca93ea95998ab76b2de6
Alicia Baker <aliciabaker314@gmail.com>
2018-12-06 14:58:00 -0700

    some controller updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,24 @@
 class SessionsController < ApplicationController
 
   def new
+
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in"
+      flash[:success] = "Logged in successfully"
       redirect_to user_path(user)
     else
-      flash.now[:danger] = "There was soemthing wrong with your login"
+      flash.now[:danger] = "There was something wrong with the login information"
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have logged out"
+    flash[:success] = "You have successfully logged out"
     redirect_to root_path
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,84 +1,72 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :require_admin, only: [:destroy]
-  # GET /users
-  # GET /users.json
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-  end
-
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
   def create
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:success] = "Welcome to The Backpack Blazer, #{@user.username}!"
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to user_path(@user)
     else
       render 'new'
     end
   end
 
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:success] = "Your account was updated successfully"
-        redirect_to articles_path
-      else
-        render 'edit'
-      end
+  def edit
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    flash[:danger] = "User and all articles covered by user have been deleted."
+    flash[:danger] = "User and all articles created by user have been deleted"
     redirect_to users_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:username, :email)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    def require_same_user
-      if current_user != @user and !current_user.admin?
-        flash[:danger] = "You can only edit your own Account."
-        redirect_to root_path
-      end
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
-    def require_admin
-      if logged_in? and !current_user.admin?
-        flash[:danger] = "Only Admins can perform that action."
-        redirect_to root_path
-      end
+  def require_same_user
+    if current_user != @user && !current_user.admin?
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
     end
+  end
+
+  def require_admin
+    if logged_in? && !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
+      redirect_to root_path
+    end
+  end
+
 end
